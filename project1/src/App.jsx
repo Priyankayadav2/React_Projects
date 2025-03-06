cf66f74f8771cbfe8977a59aca9074f2cd75cfa2
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2025-03-06 07:16:01 +0530

    Initial commit

---
+++ project1/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navigation from "./components/Navigation";
+import Women from "./components/ContentSection";
+import ContentSection from "./components/ContentSection";
+const App=()=>{
+  return(
+  <div>
+    <Navigation />
+    <ContentSection />
+  </div>
+  );
+};
+export default App;
